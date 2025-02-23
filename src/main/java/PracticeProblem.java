d9f9d4399622d3957f12dba4df285c8410bfeddb
ilakkiyan-uruthireswaran-ics4u1-3-25 <Ilakkiyan.uruthireswaran@student.tdsb.on.ca>
2025-02-23 19:49:35 +0000

    passed

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,5 +1,21 @@
 public class PracticeProblem {
 
+	public static String pyramid(int x) {
+		String k = "";
+		for (int i = 1; i <= x; i++) {
+			for (int j = 0; j < x - i; j++) {
+				k += " ";
+			}
+			for (int j = -1; j < (1 * i - 1); j++) {
+				k += "* ";
+			}
+			k += "\n";
+	}
+	return k;
+}
+
+	
+
 	public static void main(String args[]) {
 
 	}
